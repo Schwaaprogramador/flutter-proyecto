865d0ca7f3e88d03dd7344ce165cd172d5ee685f
Alnago-front <frontd@alnago.com>
2024-06-15 10:29:21 -0500

    historial

--- proyecto_calculadora_1/lib/main.dart
+++ proyecto_calculadora_1/lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 void main(List<String> args) {
   runApp(const CalculadoraApp());
@@ -9,22 +10,51 @@ class CalculadoraApp extends StatefulWidget {
   const CalculadoraApp({super.key});
 
   @override
-  State <CalculadoraApp> createState() =>  CalculadoraAppState();
+  State<CalculadoraApp> createState() =>  CalculadoraAppState();
 }
 
 //El estado
-class  CalculadoraAppState extends State <CalculadoraApp> {
+class CalculadoraAppState extends State<CalculadoraApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Calculadora",
       home: Scaffold(
-        backgroundColor:const Color(0xFF283637),
-        appBar: AppBar(),
+        backgroundColor:const Color.fromARGB(255, 13, 213, 228),
         body: Container(
           padding: const EdgeInsets.all(12),
-          child: Column(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 12), 
+                  child: Text("Historial", style: GoogleFonts.rubik(
+                      textStyle: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white
+                        )
+                      )
+                    ),
+                  ),
+              ),
+              Container(
+                alignment: const Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: (
+                    Text("X", style: GoogleFonts.rubik(
+                      textStyle: const TextStyle(
+                        fontSize: 48,
+                        color: Colors.white
+                      )
+                    ),)
+                  ),
+                  )
+              )
+            ],
+          ),
         ),
       )
     );
